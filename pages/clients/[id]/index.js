9dd322059cf57f8bc86325873921c1f836d5393e
Vanessa <interntemp21@gmail.com>
2021-12-22 10:04:11 +0800

    page and file-based routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,19 @@ function ClientProjectPage(){
     const router = useRouter();
 
     console.log(router.query);
+
+    function loadProjectHandler(){
+        //load data...
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id:'max' , clientprojectid: 'projecta'},
+        });
+    }
+
     return(
         <div>
             <h1>The Client Project Page</h1>
+            <button onClick={loadProjectHandler}>Load Project</button>
         </div>
     );
 }
